c9c76e532fcf730369b63087aa94b9d5891a921f
Doug MacEwen <doug@glass-imaging.com>
2021-11-10 14:16:38 -0800

    Stream 1920x1080 from capture card and flip image

--- jetcam/usb_camera.py
+++ jetcam/usb_camera.py
@@ -15,6 +15,8 @@ class USBCamera(Camera):
     
     def __init__(self, *args, **kwargs):
         super(USBCamera, self).__init__(*args, **kwargs)
+
+        print(f'Capture FPS :: {self.capture_fps} | capture_width :: {self.capture_width} | capture_height :: {self.capture_height} | capture_device :: {self.capture_device}')
         try:
             self.cap = cv2.VideoCapture(self._gst_str(), cv2.CAP_GSTREAMER)
 
@@ -30,12 +32,17 @@ class USBCamera(Camera):
         atexit.register(self.cap.release)
                 
     def _gst_str(self):
-        return 'v4l2src device=/dev/video{} ! video/x-raw, width=(int){}, height=(int){}, framerate=(fraction){}/1 ! videoconvert !  video/x-raw, format=(string)BGR ! appsink'.format(self.capture_device, self.capture_width, self.capture_height, self.capture_fps)
+        # return 'v4l2src device=/dev/video{} ! video/x-raw, width=(int){}, height=(int){}, framerate=(fraction){}/1 ! videoconvert !  video/x-raw ! appsink'.format(self.capture_device, self.capture_width, self.capture_height, self.capture_fps)
+
+        # return 'v4l2src device=/dev/video0 io-mode=2 ! image/jpeg, framerate=30/1, width=1920, height=1080 ! appsink'
+
+        return 'v4l2src device=/dev/video0 io-mode=2 ! image/jpeg, framerate=30/1, width=1920, height=1080 ! nvjpegdec ! video/x-raw, format=(string)I420 ! nvvidconv flip-method=4 ! nvjpegenc ! appsink'
+
     
     def _read(self):
         re, image = self.cap.read()
         if re:
-            image_resized = cv2.resize(image,(int(self.width),int(self.height)))
-            return image_resized
+            # image_resized = cv2.resize(image,(int(self.width),int(self.height)))
+            return image
         else:
             raise RuntimeError('Could not read image from camera')
